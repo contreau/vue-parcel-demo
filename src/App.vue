e92d78ebac73ed3f4e99b993b3e2b8e838faba18
zenDev-2 <72581161+contreau@users.noreply.github.com>
2024-03-08 18:24:07 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import Message from "./Message.vue";
+const count = ref(0);
+const date = ref({ month: 3, day: 8, year: 2024 });
+</script>
+
+<template>
+  <Message></Message>
+  <button @click="count++">You've clicked me {{ count }} time(s).</button>
+  <p>It is {{ date.month }}/{{ date.day }}/{{ date.year }}</p>
+</template>
+
+<style lang="scss">
+body {
+  background-color: #233040;
+  color: #ffffff;
+  font-family: system-ui;
+  text-align: center;
+}
+</style>
---
+++ src/Message.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Salut.</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  color: rgb(0, 255, 162);
+}
+</style>
