3aec4df0e5a1d63f37be101adcf007f2a2ba3dc4
zenDev-2 <72581161+contreau@users.noreply.github.com>
2024-03-10 04:24:03 -0400

    updated config

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
 import { ref } from "vue";
 import Message from "./Message.vue";
 const count = ref(0);
-const date = ref({ month: "March", day: 9, year: 2024 });
+const date = ref({
+  month: new Date().getMonth() + 1,
+  day: new Date().getDate(),
+  year: new Date().getFullYear(),
+});
 </script>
 
 <template>
   <Message></Message>
   <button @click="count++">You've clicked me {{ count }} time(s).</button>
-  <p>It is {{ date.month }} {{ date.day }}, {{ date.year }}</p>
+  <p>It is {{ date.month }}/{{ date.day }}/{{ date.year }}</p>
 </template>
 
 <style>
